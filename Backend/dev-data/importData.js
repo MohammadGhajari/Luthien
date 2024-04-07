2f32e0bfb829993d6e675b054fa47ae412ac20a6
Mohammad Ghajari <stormborndaenerys0@gmail.com>
2024-04-08 01:26:55 +0330

    add some test data

---
+++ Backend/dev-data/importData.js
@@ -0,0 +1,56 @@
+const dotenv = require('dotenv');
+const fs = require('fs');
+dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
+const Hotel = require('./../model/hotelModel');
+const User = require('./../model/userModel');
+const Review = require('./../model/reviewModel');
+const Room = require('./../model/roomModel');
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log('connected to database');
+  });
+
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf8'));
+const hotels = JSON.parse(fs.readFileSync(`${__dirname}/hotels.json`, 'utf8'));
+const rooms = JSON.parse(fs.readFileSync(`${__dirname}/rooms.json`, 'utf8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf8'),
+);
+
+const importData = async () => {
+  try {
+    await User.create(users);
+    await Hotel.create(hotels);
+    await Room.create(rooms);
+    await Review.create(reviews);
+    console.log('data imported successfully');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit(0);
+};
+
+const deleteData = async () => {
+  try {
+    await User.deleteMany({});
+    await Hotel.deleteMany({});
+    await Room.deleteMany({});
+    await Review.deleteMany({});
+    console.log('data deleted successfully');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit(0);
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
--- Frontend/Luthien/src/App.jsx
+++ Frontend/Luthien/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  useEffect(async () => {
+    try {
+      const data = {
+        stars: 3
+      }
+      const response = await axios.delete('http://127.0.0.10:8000/api/hotels/6612cf0fec9a2f126834703b');
+      console.log(response);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }, []); 
+  
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <h1>This is a react app</h1>
   )
 }
 
